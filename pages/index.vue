e9f09f7313ff531b9fde1b5c1fa5251daf33f5be
Lana Schuster <lanaschuster1@gmail.com>
2021-10-08 22:55:41 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <UilVuejs size="180px" class="logo" />
+</template>
+
+<script>
+import { UilVuejs } from '@iconscout/vue-unicons'
+
+export default {
+  components: {
+    UilVuejs
+  }
+}
+</script>
