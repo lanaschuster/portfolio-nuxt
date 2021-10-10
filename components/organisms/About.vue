0e35bc6a073058b175f5c0bf10b502597303d735
Lana Schuster <lanaschuster1@gmail.com>
2021-10-09 21:08:27 -0300

    about section

---
+++ components/organisms/About.vue
@@ -0,0 +1,34 @@
+<template>
+  <section id="about" ref="about" class="about section">
+    <h2 class="section__title">About Me</h2>
+    <span class="section__subtitle">My Introduction</span>
+
+    <div class="about__container container grid">
+      <img src="" alt="" class="about__img">
+      <div class="about__data">
+        <p class="about__description">
+          Web developer, with extensive knowledge and years of experience, working in web technologies and delivering quality work.
+        </p>
+        <div class="about__info">
+          <div>
+            <span class="about__info-title">03+</span>
+            <span class="about__info-name">Anos de <br>experiência</span>
+          </div>
+          <div>
+            <span class="about__info-title">01+</span>
+            <span class="about__info-name">Projetos <br>concluídos</span>
+          </div>
+          <div>
+            <span class="about__info-title">02+</span>
+            <span class="about__info-name">Empresas <br>trabalhadas</span>
+          </div>
+        </div>
+        <div class="about__btns">
+          <a download="" href="" class="button button--flex">
+            Download CV <i class="uil uil-download-alt button__icon"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
