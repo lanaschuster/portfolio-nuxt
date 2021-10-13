97d1df2a6751cb3cc29906d9e0bef56306421742
Lana Schuster <lanaschuster1@gmail.com>
2021-10-12 21:07:03 -0300

    footer

---
+++ components/molecules/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+  <footer class="footer">
+    <div class="footer__bg">
+      <div class="footer__container container grid">
+        <div>
+          <h1 class="footer__title">Lana</h1>
+          <span class="footer__subtitle">Full Stack Developer</span>
+        </div>
+
+        <ul class="footer__links">
+          <li>
+            <a href="#services" class="footer__link">Services</a>
+          </li>
+          <li>
+            <a href="#portfolio" class="footer__link">Portfolio</a>
+          </li>
+          <li>
+            <a href="#contact" class="footer__link">Contact Me</a>
+          </li>
+        </ul>
+
+        <div class="footer__socials">
+          <a href="" target="_blank" class="footer__social">
+            <i class="uil uil-facebook-f"></i>
+          </a>
+          <a href="" target="_blank" class="footer__social">
+            <i class="uil uil-twitter"></i>
+          </a>
+          <a href="" target="_blank" class="footer__social">
+            <i class="uil uil-linkedin"></i>
+          </a>
+          <a href="" target="_blank" class="footer__social">
+            <i class="uil uil-telegram"></i>
+          </a>
+        </div>
+      </div>
+
+      <p class="footer__copy">&#169; Lana Schuster. All right reserved.</p>
+    </div>
+  </footer>
+</template>
--- layouts/index.vue
+++ layouts/index.vue
@@ -2,17 +2,20 @@
   <div>
     <Header />
     <Main />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '@/components/molecules/Header.vue'
+import Footer from '@/components/molecules/Footer.vue'
 import Main from '@/components/organisms/Main.vue'
 
 export default {
   components: {
     Header,
-    Main
+    Main,
+    Footer
   }
 }
 </script>
