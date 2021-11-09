5cb3e1dc5b8fea3b26da946158af68986e7fbe6c
Lana Schuster <lanaschuster1@gmail.com>
2021-11-09 10:26:26 -0300

    enviando mensagem pelo form

--- components/organisms/ContactMe.vue
+++ components/organisms/ContactMe.vue
@@ -18,33 +18,38 @@
         </div>
       </div>
 
-      <form action="" class="contact__form grid">
+      <form class="contact__form grid">
         <div class="contact__inputs grid">
           <div class="contact__content">
-            <label for="" class="contact__label">Nome</label>
-            <input type="text" class="contact__input" />
+            <label for="" class="contact__label">Nome *</label>
+            <input v-model="form.name" type="text" class="contact__input" required />
           </div>
           <div class="contact__content">
-            <label for="" class="contact__label">E-mail</label>
-            <input type="email" class="contact__input" />
+            <label for="" class="contact__label">E-mail *</label>
+            <input v-model="form.email" type="email" class="contact__input" required />
           </div>
         </div>
         <div class="contact__content">
           <label for="" class="contact__label">Projeto</label>
-          <input type="text" class="contact__input" />
+          <input v-model="form.project" type="text" class="contact__input" />
         </div>
         <div class="contact__content">
-          <label for="" class="contact__label">Mensagem</label>
+          <label for="" class="contact__label">Mensagem *</label>
           <textarea
             id=""
+            v-model="form.message"
             name=""
             cols="0"
             rows="7"
             class="contact__input"
+            required
           ></textarea>
         </div>
         <div>
-          <a href="#" class="button button--flex">
+          <p class="contact__label">* campos de preenchimento obrigatório</p>
+        </div>
+        <div>
+          <a class="button button--flex" @click="validate">
             Enviar mensagem
             <i class="uil uil-message button__icon"></i>
           </a>
@@ -65,6 +70,71 @@ export default {
   data() {
     return {
       contactSection: {},
+      form: {
+        name: '',
+        email: '',
+        project: '',
+        message: ''
+      }
+    }
+  },
+  methods: {
+    validate() {
+      if (!this.form.name) {
+        alert('Preencha o campo nome')
+        return
+      }
+
+      if (!this.form.email) {
+        alert('Preencha o campo email')
+        return
+      }
+
+      if (!this.form.message) {
+        alert('Preencha a mensagem que deseja enviar')
+        return
+      }
+
+      this.sendMessage()
+    },
+    sendMessage() {
+      this.$apollo.mutate({
+        mutation: gql`
+          mutation(
+            $name: String!
+            $email: String!
+            $project: String
+            $message: String!
+          ) {
+            createMensagem(
+              input: {
+                data: {
+                  name: $name,
+                  email: $email,
+                  project: $project,
+                  message: $message
+                }
+              }
+            ) {
+              mensagem {
+                id
+              }
+            }
+          }
+        `,
+        variables: {
+          name: this.form.name,
+          email: this.form.email,
+          project: this.form.project,
+          message: this.form.message,
+        }
+      })
+      .then(() => { 
+        alert('Mensagem enviada com sucesso! :D');
+      })
+      .catch(() => { 
+        alert('Desculpa, não foi possível enviar a sua mensagem :(')
+      })
     }
   },
   apollo: {
